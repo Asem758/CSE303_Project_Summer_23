858a0f62be70ab06e5b9ebfe659be9a1f320b604
Ashrafur Rahman Asem <79334393+Asem758@users.noreply.github.com>
2023-11-03 19:32:40 +0600

    DBMS frontend and backend updated

--- index.js
+++ index.js
@@ -1,266 +1,258 @@
-/*!
- * type-is
- * Copyright(c) 2014 Jonathan Ong
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const mysql = require('mysql');
+app.use(cors())
+app.use(express.json())
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "lsbdata"
 
-'use strict'
 
-/**
- * Module dependencies.
- * @private
- */
+})
+app.get('/temp', (req, res) => {
 
-var typer = require('media-typer')
-var mime = require('mime-types')
 
-/**
- * Module exports.
- * @public
- */
+    const querry = ` SELECT * FROM lonkabangla.customer_table; `
 
-module.exports = typeofrequest
-module.exports.is = typeis
-module.exports.hasBody = hasbody
-module.exports.normalize = normalize
-module.exports.match = mimeMatch
 
-/**
- * Compare a `value` content-type with `types`.
- * Each `type` can be an extension like `html`,
- * a special shortcut like `multipart` or `urlencoded`,
- * or a mime type.
- *
- * If no types match, `false` is returned.
- * Otherwise, the first `type` that matches is returned.
- *
- * @param {String} value
- * @param {Array} types
- * @public
- */
 
-function typeis (value, types_) {
-  var i
-  var types = types_
+    db.query(querry, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        }
 
-  // remove parameters and normalize
-  var val = tryNormalizeType(value)
 
-  // no type or invalid
-  if (!val) {
-    return false
-  }
+        //res.send(result)
+    })
 
-  // support flattened arguments
-  if (types && !Array.isArray(types)) {
-    types = new Array(arguments.length - 1)
-    for (i = 0; i < types.length; i++) {
-      types[i] = arguments[i + 1]
-    }
-  }
 
-  // no types, return the content type
-  if (!types || !types.length) {
-    return val
-  }
+})
+app.post('/createAcc', (req, res) => {
+    const data = req.body;
 
-  var type
-  for (i = 0; i < types.length; i++) {
-    if (mimeMatch(normalize(type = types[i]), val)) {
-      return type[0] === '+' || type.indexOf('*') !== -1
-        ? val
-        : type
-    }
-  }
+    console.log(data)
 
-  // no matches
-  return false
-}
+    const querry1 = `INSERT INTO lonkabangla.customer_table VALUES ("124","${data.presentaddress}","${data.email}","${data.phnnum}","${data.bodID}","${data.nationality}","in123","${data.acontNum}","not","not","${data.ocupation}" ); `
 
-/**
- * Check if a request has a request body.
- * A request with a body __must__ either have `transfer-encoding`
- * or `content-length` headers set.
- * http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.3
- *
- * @param {Object} request
- * @return {Boolean}
- * @public
- */
 
-function hasbody (req) {
-  return req.headers['transfer-encoding'] !== undefined ||
-    !isNaN(req.headers['content-length'])
-}
+    const querry2 = `INSERT INTO lonkabangla.customer_table (presentAddress,customer_email, customer_phn,customerDOB, nationality,inverstorID,BankACC, BO_ID, HeadOfSattlement,occupation   )
+    VALUES ("${data.presentaddress}","${data.email}","${data.phnnum}","${data.bodID}","${data.nationality}","in123","${data.acontNum}","not","not","${data.ocupation}" );`
 
-/**
- * Check if the incoming request contains the "Content-Type"
- * header field, and it contains any of the give mime `type`s.
- * If there is no request body, `null` is returned.
- * If there is no content type, `false` is returned.
- * Otherwise, it returns the first `type` that matches.
- *
- * Examples:
- *
- *     // With Content-Type: text/html; charset=utf-8
- *     this.is('html'); // => 'html'
- *     this.is('text/html'); // => 'text/html'
- *     this.is('text/*', 'application/json'); // => 'text/html'
- *
- *     // When Content-Type is application/json
- *     this.is('json', 'urlencoded'); // => 'json'
- *     this.is('application/json'); // => 'application/json'
- *     this.is('html', 'application/*'); // => 'application/json'
- *
- *     this.is('html'); // => false
- *
- * @param {String|Array} types...
- * @return {String|false|null}
- * @public
- */
 
-function typeofrequest (req, types_) {
-  var types = types_
+    db.query(querry2, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
 
-  // no body
-  if (!hasbody(req)) {
-    return null
-  }
 
-  // support flattened arguments
-  if (arguments.length > 2) {
-    types = new Array(arguments.length - 1)
-    for (var i = 0; i < types.length; i++) {
-      types[i] = arguments[i + 1]
-    }
-  }
+        //res.send(result)
+    })
+})
 
-  // request content type
-  var value = req.headers['content-type']
 
-  return typeis(value, types)
-}
 
-/**
- * Normalize a mime type.
- * If it's a shorthand, expand it to a valid mime type.
- *
- * In general, you probably want:
- *
- *   var type = is(req, ['urlencoded', 'json', 'multipart']);
- *
- * Then use the appropriate body parsers.
- * These three are the most common request body types
- * and are thus ensured to work.
- *
- * @param {String} type
- * @private
- */
 
-function normalize (type) {
-  if (typeof type !== 'string') {
-    // invalid type
-    return false
-  }
 
-  switch (type) {
-    case 'urlencoded':
-      return 'application/x-www-form-urlencoded'
-    case 'multipart':
-      return 'multipart/*'
-  }
 
-  if (type[0] === '+') {
-    // "+json" -> "*/*+json" expando
-    return '*/*' + type
-  }
+app.post('/provideBoID', (req, res) => {
+    const data = req.body;
 
-  return type.indexOf('/') === -1
-    ? mime.lookup(type)
-    : type
-}
+    console.log(data)
 
-/**
- * Check if `expected` mime type
- * matches `actual` mime type with
- * wildcard and +suffix support.
- *
- * @param {String} expected
- * @param {String} actual
- * @return {Boolean}
- * @private
- */
 
-function mimeMatch (expected, actual) {
-  // invalid type
-  if (expected === false) {
-    return false
-  }
 
-  // split types
-  var actualParts = actual.split('/')
-  var expectedParts = expected.split('/')
+    const querry2 = `
+    UPDATE   lonkabangla.customer_table
+    SET BO_ID = ${data.provideID} WHERE customerCode=${data.id} ; `
 
-  // invalid format
-  if (actualParts.length !== 2 || expectedParts.length !== 2) {
-    return false
-  }
 
-  // validate type
-  if (expectedParts[0] !== '*' && expectedParts[0] !== actualParts[0]) {
-    return false
-  }
+    db.query(querry2, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
 
-  // validate suffix wildcard
-  if (expectedParts[1].substr(0, 2) === '*+') {
-    return expectedParts[1].length <= actualParts[1].length + 1 &&
-      expectedParts[1].substr(1) === actualParts[1].substr(1 - expectedParts[1].length)
-  }
 
-  // validate subtype
-  if (expectedParts[1] !== '*' && expectedParts[1] !== actualParts[1]) {
-    return false
-  }
+        //res.send(result)
+    })
+})
 
-  return true
-}
 
-/**
- * Normalize a type and remove parameters.
- *
- * @param {string} value
- * @return {string}
- * @private
- */
 
-function normalizeType (value) {
-  // parse the type
-  var type = typer.parse(value)
+app.post('/sendhsapprovefrist', (req, res) => {
+    const data = req.body;
+    const frist = data.frist
+    const join = data.join
+    const customerinfo = data.customerinfo
 
-  // remove the parameters
-  type.parameters = undefined
+    console.log(frist)
 
-  // reformat it
-  return typer.format(type)
-}
+    const querry1 = `insert into lonkabangla.firstacc
+    values(${customerinfo.BONUM},"${frist.email2}","${frist.phnnum2}","${frist.nationality2}","${frist.BOD2}",
+    "${frist.presentaddress2}");`
 
-/**
- * Try to normalize a type and remove parameters.
- *
- * @param {string} value
- * @return {string}
- * @private
- */
 
-function tryNormalizeType (value) {
-  if (!value) {
-    return null
-  }
+    const querry2 = `insert into lonkabangla.jointaccount
+    values(${customerinfo.BONUM},'${join.email1}','${join.phnnum1}','${join.nationality1}',"${join.BOD1}","${join.presentaddress1}","${join.ocupation1}");`
 
-  try {
-    return normalizeType(value)
-  } catch (err) {
-    return null
-  }
-}
+
+    db.query(querry1, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
+
+})
+
+app.post('/sendhsapprovejoin', (req, res) => {
+    const data = req.body;
+    const frist = data.frist
+    const join = data.join
+    const customerinfo = data.customerinfo
+
+    console.log(join)
+
+    const querry1 = `insert into lonkabangla.firstacc
+    values(${customerinfo.BONUM},"${frist.email2}","${frist.phnnum2}","${frist.nationality2}","${frist.BOD2}",
+    "${frist.presentaddress2}");`
+    const querry2 = `insert into lonkabangla.jointaccount
+    values(${customerinfo.BONUM},'${join.email1}','${join.phnnum1}','${join.nationality1}',"${join.BOD1}","${join.presentaddress1}","${join.ocupation1}");`
+
+
+    // db.query(querry1, (err, result) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         res.send(result);
+    //     }
+    // })
+    db.query(querry2, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
+})
+app.post('/HSupdate', (req, res) => {
+    const data = req.body;
+    console.log(data)
+    const querry1 = `UPDATE  lonkabangla.customer_table
+    SET HeadOfSattlement = '${data.approve}' WHERE customerCode=${data.id} ;  `
+    db.query(querry1, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
+})
+
+
+app.get('/hello', (req, res) => {
+    const querry = 'SELECT * FROM lonkabangla.customer_table;'
+    db.query(querry, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        }
+
+
+        //res.send(result)
+    })
+
+
+
+
+
+})
+app.get('/customerdetail', (req, res) => {
+    const querry = 'SELECT customerCode,customer_email,BankACC,BO_ID, HeadOfSattlement FROM lonkabangla.customer_table;'
+    db.query(querry, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        }
+
+
+
+    })
+
+
+})
+app.get('/BOmngr', (req, res) => {
+    const querry = 'SELECT customerCode,customer_email,BankACC,BO_ID,HeadOfSattlement FROM lonkabangla.customer_table;'
+    db.query(querry, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        }
+
+
+        //res.send(result)
+    })
+
+
+})
+
+
+
+
+app.get('/HSfech', (req, res) => {
+
+
+    const querry = `select customerCode,FEmail,JFatherEmail  from
+    (select *from lonkabangla.jointaccount INNER JOIN lonkabangla.firstacc ON 	jointaccount.Jcustomercode = firstacc.FcustomerCode) AS temp1 inner join lonkabangla.customer_table 
+    on temp1.Jcustomercode = customer_table.customerCode `
+
+
+
+    db.query(querry, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        }
+
+
+        //res.send(result)
+    })
+
+
+})
+
+app.get('/', (req, res) => {
+    res.send([{ value: 100 }]);
+})
+
+app.get('/users', (req, res) => {
+    res.send([{ value: 100 }]);
+})
+app.listen(port, () => {
+    console.log("listening to port ", port)
+})
